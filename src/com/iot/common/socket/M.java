4922011ad10831aeac4089a6f39473595da4577c
wutongpeng <1519081858@qq.com>
2016-04-26 13:06:04 +0800

    完成用线程控制socket通信

--- src/com/iot/common/socket/M.java
+++ src/com/iot/common/socket/M.java
@@ -1,13 +1,20 @@
 package com.iot.common.socket;
 
+import java.util.Timer;
+
+import org.springframework.stereotype.Component;
+
+import com.iot.common.socket.task.MyTask;
+
 /**
  * 开启一个任务
  * */
-//@Component
+@Component
 public class M {
 //	public static void main(String[] args) {
-//	//public M(){
-//		//Timer timer = new Timer();		
-//		//timer.schedule(new MyTask(), 1000 ,5000);
-//	}
+	public M(){
+		System.out.println("M");
+		Timer timer = new Timer();		
+		timer.schedule(new MyTask(), 1000 ,5000);
+	}
 }
